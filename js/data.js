89fc353c87e4d056d01e115a3939f16192ca0b94
HarshitSinghSomvanshi <69684232+HarshitSinghSomvanshi@users.noreply.github.com>
2025-12-29 10:41:56 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const marketData = {
+  "Data Analyst": {
+    skills: { SQL: 80, Python: 70, PowerBI: 60 },
+    salary: 85000,
+    roadmap: [
+      "Advanced SQL",
+      "Python for Analytics",
+      "Machine Learning Basics",
+      "Cloud Analytics"
+    ]
+  },
+  "Software Engineer": {
+    skills: { JavaScript: 75, React: 65, DSA: 70 },
+    salary: 105000,
+    roadmap: [
+      "System Design",
+      "Advanced React",
+      "Backend APIs",
+      "Cloud Deployment"
+    ]
+  }
+};
